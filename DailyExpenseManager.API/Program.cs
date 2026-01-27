a8a183b62befada5105ee8c6615279cd542737de
Chirag Baroliya <87845461+ChiragBaroliya@users.noreply.github.com>
2026-01-27 08:58:07 +0530

    Commit Code

--- DailyExpenseManager.API/Program.cs
+++ DailyExpenseManager.API/Program.cs
@@ -122,12 +122,8 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
