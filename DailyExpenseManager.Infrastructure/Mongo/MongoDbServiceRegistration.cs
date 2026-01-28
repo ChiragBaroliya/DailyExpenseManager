62433dbda28a610e62ab9a3ef7901da8bf2d7821
Chirag Baroliya <87845461+ChiragBaroliya@users.noreply.github.com>
2026-01-28 08:50:00 +0530

    Code committed

--- DailyExpenseManager.Infrastructure/Mongo/MongoDbServiceRegistration.cs
+++ DailyExpenseManager.Infrastructure/Mongo/MongoDbServiceRegistration.cs
@@ -1,6 +1,7 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
 using System;
 
 namespace DailyExpenseManager.Infrastructure.Mongo;
@@ -9,34 +10,44 @@ public static class MongoDbServiceRegistration
 {
     public static IServiceCollection AddMongoDb(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString =
-       Environment.GetEnvironmentVariable("MONGODB__CONNECTION__STRING")
-       ?? configuration["MONGODB_CONNECTION_STRING"];
+        try
+        {
+            var connectionString =
+     Environment.GetEnvironmentVariable("MONGODB__CONNECTION__STRING")
+     ?? configuration["MONGODB_CONNECTION_STRING"];
 
-        var databaseName =
-            Environment.GetEnvironmentVariable("MONGODB__DATABASE__NAME")
-            ?? configuration["MONGODB_DATABASE_NAME"];
+            var databaseName =
+                Environment.GetEnvironmentVariable("MONGODB__DATABASE__NAME")
+                ?? configuration["MONGODB_DATABASE_NAME"];
 
-        if (string.IsNullOrWhiteSpace(connectionString))
-            throw new Exception("MongoDB connection string is missing");
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new Exception("MongoDB connection string is missing");
 
-        if (string.IsNullOrWhiteSpace(databaseName))
-            throw new Exception("MongoDB database name is missing");
+            if (string.IsNullOrWhiteSpace(databaseName))
+                throw new Exception("MongoDB database name is missing");
 
-        Console.WriteLine($"MongoDB Database: {databaseName}");
+            Console.WriteLine($"MongoDB Database: {databaseName}");
+            Console.WriteLine($"MongoDB ConnectionString: {connectionString}");
 
-        var mongoSettings = MongoClientSettings.FromConnectionString(connectionString);
-        mongoSettings.ServerSelectionTimeout = TimeSpan.FromSeconds(5);
-        mongoSettings.ConnectTimeout = TimeSpan.FromSeconds(5);
+            var mongoSettings = MongoClientSettings.FromConnectionString(connectionString);
+            mongoSettings.ServerSelectionTimeout = TimeSpan.FromSeconds(5);
+            mongoSettings.ConnectTimeout = TimeSpan.FromSeconds(5);
+            mongoSettings.UseTls = true;
 
-        var client = new MongoClient(mongoSettings);
+            var client = new MongoClient(mongoSettings);
 
-        services.AddSingleton(client);
-        services.AddScoped(sp => client.GetDatabase(databaseName));
+            services.AddSingleton(client);
+            services.AddScoped(sp => client.GetDatabase(databaseName));
 
-        services.AddScoped<Repositories.IUserRepository, Repositories.UserRepository>();
-        services.AddScoped<Repositories.ICategoryRepository, Repositories.CategoryRepository>();
+            services.AddScoped<Repositories.IUserRepository, Repositories.UserRepository>();
+            services.AddScoped<Repositories.ICategoryRepository, Repositories.CategoryRepository>();
 
+           
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
         return services;
     }
 }
